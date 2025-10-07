b9222e16cf03ae9d70a3ef634a4bd242db14a72d
Hdz-JC <a22150084@itmexicali.edu.mx>
2025-10-07 11:38:52 -0700

    Second version ahora si

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../controllers/auth_controller.dart';
+import 'package:go_router/go_router.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
@@ -12,12 +13,33 @@ class LoginView extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: const Text('Iniciar Sesión')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            auth.login(); // Cambia el estado a "logueado"
-          },
-          child: const Text('Entrar a la app'),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TextField(
+              decoration: InputDecoration(labelText: 'Correo'),
+            ),
+            const SizedBox(height: 10),
+            const TextField(
+              decoration: InputDecoration(labelText: 'Contraseña'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                auth.login(); // simula login
+              },
+              child: const Text('Ingresar'),
+            ),
+            TextButton(
+              onPressed: () {
+                context.go('/registro'); // navega a la ventana de registro
+              },
+              child: const Text('¿No tienes cuenta? Regístrate'),
+            ),
+          ],
         ),
       ),
     );
